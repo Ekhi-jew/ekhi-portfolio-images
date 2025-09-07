5ee86a18d4a941a88e0d391ac6dd48669068ef47
Ekhi-jew <ekhig1987@gmail.com>
2025-09-07 10:28:14 +0200

    Update dense.js

--- Dense/dense.js
+++ Dense/dense.js
@@ -1,15 +1,15 @@
 {
   "dense": [
-    "https://github.com/Ekhi-jew/ekhi-portfolio-images/raw/main/Dense/Anillo-oro-.jpg",
-    "https://github.com/Ekhi-jew/ekhi-portfolio-images/raw/main/Dense/Anillo-plata-mod-.jpg",
-    "https://github.com/Ekhi-jew/ekhi-portfolio-images/raw/main/Dense/Pendientes Dense Dorados mod 2.jpg",
-    "https://github.com/Ekhi-jew/ekhi-portfolio-images/raw/main/Dense/Pendientes-Dense-Textura-Dorados-3.jpg",
-    "https://github.com/Ekhi-jew/ekhi-portfolio-images/raw/main/Dense/Pendientes-oro-2-.jpg",
-    "https://github.com/Ekhi-jew/ekhi-portfolio-images/raw/main/Dense/Pendientes-plata-2-mod-.jpg",
-    "https://github.com/Ekhi-jew/ekhi-portfolio-images/raw/main/Dense/Pendientes-plata-mod-.jpg",
-    "https://github.com/Ekhi-jew/ekhi-portfolio-images/raw/main/Dense/Pendientes-tex--plata-3-mod-.jpg",
-    "https://github.com/Ekhi-jew/ekhi-portfolio-images/raw/main/Dense/Pendientes-tex--plata-mod-.jpg",
-    "https://github.com/Ekhi-jew/ekhi-portfolio-images/raw/main/Dense/Pendientes-textura-dorados-2-mod.jpg",
-    "https://github.com/Ekhi-jew/ekhi-portfolio-images/raw/main/Dense/Pendientes-textura-dorados-3.jpg"
+    "https://github.com/Ekhi-jew/ekhi-portfolio-images/blob/main/Dense/Anillo-oro-.jpg?raw=true",
+    "https://github.com/Ekhi-jew/ekhi-portfolio-images/blob/main/Dense/Anillo-plata-mod-.jpg?raw=true",
+    "https://github.com/Ekhi-jew/ekhi-portfolio-images/blob/main/Dense/Pendientes Dense Dorados mod 2.jpg?raw=true",
+    "https://github.com/Ekhi-jew/ekhi-portfolio-images/blob/main/Dense/Pendientes-Dense-Textura-Dorados-3.jpg?raw=true",
+    "https://github.com/Ekhi-jew/ekhi-portfolio-images/blob/main/Dense/Pendientes-oro-2-.jpg?raw=true",
+    "https://github.com/Ekhi-jew/ekhi-portfolio-images/blob/main/Dense/Pendientes-plata-2-mod-.jpg?raw=true",
+    "https://github.com/Ekhi-jew/ekhi-portfolio-images/blob/main/Dense/Pendientes-plata-mod-.jpg?raw=true",
+    "https://github.com/Ekhi-jew/ekhi-portfolio-images/blob/main/Dense/Pendientes-tex--plata-3-mod-.jpg?raw=true",
+    "https://github.com/Ekhi-jew/ekhi-portfolio-images/blob/main/Dense/Pendientes-tex--plata-mod-.jpg?raw=true",
+    "https://github.com/Ekhi-jew/ekhi-portfolio-images/blob/main/Dense/Pendientes-textura-dorados-2-mod.jpg?raw=true",
+    "https://github.com/Ekhi-jew/ekhi-portfolio-images/blob/main/Dense/Pendientes-textura-dorados-3.jpg?raw=true"
   ]
 }
